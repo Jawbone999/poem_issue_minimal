4737656205cbcce33908bba3be89c8827f548ee5
Jawbone999 <OfficialJawbone999@gmail.com>
2023-07-28 09:14:46 -0500

    solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::net::Ipv4Addr;
+use std::{net::Ipv4Addr, sync::Arc};
 
-use poem::{listener::TcpListener, Server, Route};
+use poem::{listener::TcpListener, Server, Route, EndpointExt, IntoEndpoint, http::Method, Endpoint};
 use poem_openapi::{OpenApiService, OpenApi, payload::PlainText, Tags};
 
 #[derive(Tags)]
@@ -20,14 +20,26 @@ impl Api {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let api_service =
-        OpenApiService::new(Api, "Example API", "1.0");
-    
-    let ui = api_service.swagger_ui();
+    // Solution from https://github.com/poem-web/poem/issues/613#issuecomment-1655066001
+    // Thanks!
+    let api_service = OpenApiService::new(Api, "Example API", "1.0");
+    let ui = Arc::new(api_service.swagger_ui().map_to_response());
+    let api_service_endpoint = Arc::new(api_service.into_endpoint().map_to_response());
 
+    let handler = poem::endpoint::make(move |req| {
+        let ui = ui.clone();
+        let api_service_endpoint = api_service_endpoint.clone();
+        async move {
+            if req.method() == Method::GET && req.uri().path() == "/" {
+                ui.call(req).await
+            } else {
+                api_service_endpoint.call(req).await
+            }
+        }
+    });
+    
     let app = Route::new()
-        .at("/", ui)
-        .nest("/", api_service);
+        .nest("/", handler);
 
     Server::new(TcpListener::bind((Ipv4Addr::LOCALHOST, 3000)))
         .run(app)
